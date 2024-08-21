148197a6c6335a87d8b703d59bff1f760f2c8a2b
Aditya Kulshrestha <adi.kul358@gmail.com>
2024-08-22 01:23:50 +0530

    Redirect to /dashboard

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {createBrowserRouter,RouterProvider} from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Navigate } from "react-router-dom";
 import Dashboard from './routes/Dashboard.jsx';
 import Vibe from './routes/Vibe.jsx';
 import Signup from './routes/Register.jsx';
@@ -20,26 +20,23 @@ import ChartDataLabels from 'chartjs-plugin-datalabels';
 
 Chart.register(ChartDataLabels);
 
-const Router= createBrowserRouter([
-  // {path:"/",element:<App/>},
-  {path:"/dashboard",element:<Dashboard/>},
-  {path:"/vibe",element:<Vibe/>},
-  {path:"/problem",element:<Problem/>},
-  {path:"/overview",element:<Overview/>},
-  {path:"/founder",element:<h1 className='text-2xl flex justify-center'>Yet to be made or linked to router . Configure it in main.jsx</h1>},
-  {path:"/login",element:<Login/>},
-  {path:"/register",element:<Signup/>},
-  {path:"/profile",element:<Profile/>},
-  {path:"/reviews",element:<AllReviews/>},
-  
-  
-
+const Router = createBrowserRouter([
+  { path: "/", element: <Navigate to="/dashboard" replace /> },
+  { path: "/dashboard", element: <Dashboard /> },
+  { path: "/vibe", element: <Vibe /> },
+  { path: "/problem", element: <Problem /> },
+  { path: "/overview", element: <Overview /> },
+  { path: "/founder", element: <h1 className='text-2xl flex justify-center'>Yet to be made or linked to router . Configure it in main.jsx</h1> },
+  { path: "/login", element: <Login /> },
+  { path: "/register", element: <Signup /> },
+  { path: "/profile", element: <Profile /> },
+  { path: "/reviews", element: <AllReviews /> },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-    <RouterProvider router={Router}/>
+      <RouterProvider router={Router} />
     </Provider>
   </React.StrictMode>
 )
